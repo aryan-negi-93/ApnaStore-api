83e145e6febdbfd66c828018ab2aaebdd1ad08b6
aryan-negi-93 <134124253+aryan-negi-93@users.noreply.github.com>
2024-04-09 23:42:15 +0530

    LAST

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ const Product = require('./product');
 const app = express();
 require('dotenv').config();
 
+const coreConfig = {
+    origin: "*",
+    Credential:true,
+    methods:["GET" , "POST" , "PUT" , "DELETE"]
+}
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
